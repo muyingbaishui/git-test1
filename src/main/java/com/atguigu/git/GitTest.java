615eda1a1a3b658d47f7f4c1bbbf4671409979db
muyingbaishui <2638599267@qq.com>
2022-03-01 17:07:16 +0800

    hot-fix

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,6 +4,9 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("Hello git");
-        System.out.println("Hello git");
+        System.out.println("Hello gi15646123t");
+        System.out.println("Hello gi15646123t");
+        System.out.println("Hello gi15646123t");
+        System.out.println("Hello gi15646123t");
     }
 }
