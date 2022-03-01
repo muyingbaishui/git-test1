98fb5e2a8489ca591c5ee62faede8518598a6a65
muyingbaishui <2638599267@qq.com>
2022-03-01 17:06:23 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,8 +2,9 @@ package com.atguigu.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello git");
-        System.out.println("Hello git");
-        System.out.println("Hello git");
+        System.out.println("Hello git1");
+        System.out.println("Hello git2");
+        System.out.println("Hello git3");
+        System.out.println("Hello git4");
     }
 }
